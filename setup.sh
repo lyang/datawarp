63dc604df1845299c519de9aeaeb94450299efa5
Lin Yang <github@linyang.me>
2022-11-09 08:11:10 -0800

    Obtain certificate via certbot

--- setup.sh
+++ setup.sh
@@ -2,5 +2,28 @@
 set -o errexit
 
 CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-ansible-playbook $CURRENT_DIR/playbook.yaml $ANSIBLE_OPTIONS
+CURRENT_FILE=$(basename "${BASH_SOURCE[0]}")
+
+OPTS=$(getopt --name $CURRENT_FILE --options v: --longoptions vps: --options d: --longoptions dns: -- "$@")
+eval set -- "$OPTS"
+unset OPTS
+
+while true; do
+  case $1 in
+    -v|--vps)
+      VPS=$2
+      shift 2
+      ;;
+    -d|--dns)
+      DNS=$2
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+  esac
+done
+
+ansible-playbook $CURRENT_DIR/playbook.yaml --extra-vars VPS=${VPS:-aws} --extra-vars DNS=${DNS:-cloudflare}
 echo 'Setup finished.'
